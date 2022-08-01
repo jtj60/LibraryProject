183cf35ff4ba0b94414150df9a21e95330e76f5d
jtj60 <jtj2197@gmail.com>
2022-08-01 14:51:21 -0500

    csv stuff

--- library_util.py
+++ library_util.py
@@ -1,1 +1,23 @@
-#csv stuff
+import csv
+
+def parse_user_list(filename):
+    with open(filename) as f:
+        reader = csv.DictReader( f )
+        
+        users = []
+        for row in reader:
+            user = {}
+            if not row["name"]:
+                continue 
+            
+            user['name'] = row['Name']
+            user['address'] = row['Address']
+            user['age'] = row['Age']
+            user['phone_number'] = row['Phone Number']
+            user['card_number'] = row['Card Number']
+            user['checked_out'] = row['Checked Out']
+            user['items_amount'] = row['Items Amount']
+
+            users.append(user)
+        f.close()
+    return
