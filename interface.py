919099b32b779131338f4ab794002ad50b801213
walkonies <alexwalkonies@gmail.com>
2022-07-31 23:57:04 -0500

    added tkinter gui

--- interface.py
+++ interface.py
@@ -1,19 +1,34 @@
 from library import Library
+import tkinter as tk
 
 class Interface():
+    root = tk.Tk()
+    root.title('Library App')
+    root.geometry('700x400')
+    library = None
     def __init__():
+        library = Library()
+        self.start()
 
-        #stuff goes here
+    def start(self):
+        labl = tk.Label(self.root, text='Enter library card').pack()
+        ent = tk.Entry(self.root).pack()
+        enter_button = tk.Button(self.root, text='ENTER', width=15, command=lambda:self.authenticate(ent)).pack()
+
+    def authenticate(self, entry):
+        print(entry.get())
+
 
     def main_menu(self):
+        pass
         #main menu functionality etc..
     
-    def book_request(self):
-        print('which book would you like to request?')
+    def book_request(self, user):
+        name = input('which book would you like to request?')
         #get user answer
 
-        if self.library.checkout_book(self, name):
-            printf('Success!! You have checked out '{name}'!')
+        if self.library.checkout_book( name, user):
+            print(f'Success!! You have checked out {name}!')
         else:
             print('Failure...')
             #etc....
